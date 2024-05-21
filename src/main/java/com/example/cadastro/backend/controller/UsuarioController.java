b5b42385cec676c5c8379e9805e37cdfef0b3c13
Joao Paulo <jpl71330@gmail.com>
2024-05-21 18:33:21 -0300

    Conectando Front/Back

--- src/main/java/com/example/cadastro/backend/controller/UsuarioController.java
+++ src/main/java/com/example/cadastro/backend/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.example.cadastro.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,8 @@ import com.example.cadastro.backend.entity.Usuario;
 import com.example.cadastro.backend.repository.UsuarioRepository;
 
 @RestController
-@RequestMapping("/")
+@CrossOrigin("*")
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
