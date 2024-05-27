050fa171e64524d5744e81f1abc4441da9e44aef
Joao Paulo <jpl71330@gmail.com>
2024-05-27 17:41:22 -0300

    reduzindo o código do objeto

--- src/main/java/com/example/cadastro/backend/entity/Usuario.java
+++ src/main/java/com/example/cadastro/backend/entity/Usuario.java
@@ -6,13 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Entity
 @Table(name = "usuario")
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
+
 public class Usuario {
     
     @Id
@@ -31,41 +30,4 @@ public class Usuario {
 
     @Column(name = "telefone", length = 15, nullable = true)
     private String telefone;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
 }
